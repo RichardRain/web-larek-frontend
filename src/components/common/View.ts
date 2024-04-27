a4035430b81d85cd260c161fee4d1a331b4a5938
RichardRain <alexander.hell.scorpion@gmail.com>
2024-04-27 23:52:44 +0300

    Дополнена документация проекта.

--- src/components/common/View.ts
+++ src/components/common/View.ts
@@ -13,32 +13,32 @@ export abstract class View<T> implements IView<T> {
 
   }
 
-  toggleClass(element: HTMLElement, className: string, force?: boolean) {
+  toggleClass(element: HTMLElement, className: string, force?: boolean):void {
     element.classList.toggle(className, force);
   }
 
-  protected setText(element: HTMLElement, value: unknown) {
+  protected setText(element: HTMLElement, value: unknown):void {
     if (element) {
       element.textContent = String(value);
     }
   }
 
-  setDisabled(element: HTMLElement, state: boolean) {
+  setDisabled(element: HTMLElement, state: boolean):void {
     if (element) {
       if (state) element.setAttribute('disabled', 'disabled');
       else element.removeAttribute('disabled');
     }
   }
 
-  protected setHidden(element: HTMLElement) {
+  protected setHidden(element: HTMLElement):void {
     element.style.display = 'none';
   }
 
-  protected setVisible(element: HTMLElement) {
+  protected setVisible(element: HTMLElement):void {
     element.style.removeProperty('display');
   }
 
-  protected setImage(element: HTMLImageElement, src: string, alt?: string) {
+  protected setImage(element: HTMLImageElement, src: string, alt?: string):void {
     if (element) {
       element.src = src;
       if (alt) {
